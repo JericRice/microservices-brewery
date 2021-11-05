c980817bf9f8995117b79d074c02ed881a3cbe01
Joe <jeric720@icloud.com>
2021-11-04 23:57:25 -0400

    created http mappings for POST,UPDATE,DELETE as well as logger

--- src/main/java/com/jeric/msbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/jeric/msbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jeric.msbrewery.services;
 
 import com.jeric.msbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -13,5 +15,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Beer")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo add impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer");
     }
 }
--- src/main/java/com/jeric/msbrewery/web/controller/BeerController.java
+++ src/main/java/com/jeric/msbrewery/web/controller/BeerController.java
@@ -2,16 +2,14 @@ package com.jeric.msbrewery.web.controller;
 
 import com.jeric.msbrewery.services.BeerService;
 import com.jeric.msbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RequestMapping("api/v1/beer")
+@RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
@@ -24,5 +22,31 @@ public class BeerController {
     @GetMapping({"/{beerId}"}) // curly brackets tells Spring  that this is path param
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping // Handles Creates - create new beer
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+        HttpHeaders headers = new HttpHeaders();
+
+        // todo add hostname to url
+        headers.add("Location", "/api/v1/beer" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 }
